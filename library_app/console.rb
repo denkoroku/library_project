7361da1980d1ca7129bacb7d686632f9d10392d8
denkoroku <cathriddoch@gmail.com>
2019-10-02 16:05:21 +0100

    last one?

--- library_app/console.rb
+++ library_app/console.rb
@@ -20,6 +20,8 @@ borrower2 = Borrower.new({'first_name' => 'Jane', 'last_name' => 'Hendry', 'emai
 borrower2.save()
 borrower3 = Borrower.new({'first_name' => 'Jo', 'last_name' => 'Denerley', 'email' => 'jden@denerley.co.uk'})
 borrower3.save()
+borrower4 = Borrower.new({'first_name' => 'Johnothan', 'last_name' => 'Billard', 'email' => 'jbillard@google.com'})
+borrower4.save()
 
 
 book1 = Book.new({'title' => 'The Art of Spelling', 'author' => 'Jan Smith','published_date' => '2018','bought_date' => '2019', 'price' => '8.99' })
@@ -28,9 +30,16 @@ book2 = Book.new({'title' => 'Teaching Naughty Kids', 'author' => 'Cath Riddoch'
 book2.save
 book3 = Book.new({'title' => 'Autistic Girls', 'author' => 'Helen Copus','published_date' => '2013','bought_date' => '2017', 'price' => '28.99' })
 book3.save
+book4 = Book.new({'title' => 'Slow Learning', 'author' => 'Jane Dillon','published_date' => '2014','bought_date' => '2019', 'price' => '35.00' })
+book4.save
+
 
 loan1 = Loan.new({'borrower_id' => borrower1.id, 'book_id' => book1.id, 'loan_date' => '2019-08-13 00:00:00' })
 loan1.save
+loan2 = Loan.new({'borrower_id' => borrower2.id, 'book_id' => book3.id, 'loan_date' => '2019-09-13 00:00:00' })
+loan2.save
+loan3 = Loan.new({'borrower_id' => borrower3.id, 'book_id' => book2.id, 'loan_date' => '2019-02-13 00:00:00' })
+loan3.save
 
 topic1 = Topic.new({'topic' => 'dyslexia'})
 topic1.save
@@ -40,12 +49,17 @@ topic3 = Topic.new({'topic' => 'ADHD'})
 topic3.save
 topic3 = Topic.new({'topic' => 'sensory integration'})
 topic3.save
+topic4 = Topic.new({'topic' => 'autism'})
+topic4.save
+
 
 join1 = Join.new({'topic_id' => topic1.id, 'book_id' => book1.id})
 join1.save
+join2 = Join.new({'topic_id' => topic4.id, 'book_id' => book3.id})
+join2.save
+join3 = Join.new({'topic_id' => topic1.id, 'book_id' => book2.id})
+join3.save
 
-
-# borrower1.lend(book1)
 
 
 binding.pry
